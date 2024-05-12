b5ac718d6e2edddee72330ebacc212058d00dcc9
Fabián <Fabicarbonero720@gmail.com>
2024-05-12 17:55:29 -0300

    Variables y condiciones

--- main.js
+++ main.js
@@ -5,6 +5,6 @@
 // usamos const por que es más rapida. 
 // para resumir const lo usamos solo para cuando, algo no va a cambiar y las variables cuando algo va a variar
 // const nombre = "Fabian";
-l// et edad = 28;
+// edad edad = 28;
 
-console.log
+console.log"hola mundo";
